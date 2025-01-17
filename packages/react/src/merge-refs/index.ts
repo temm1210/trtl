241466ebb407fdbbd8a2d85588e1ea5c41743ac3
temm1210 <temm1210@naver.com>
2025-01-17 19:10:44 +0900

    feat(react): create mergeRefs

---
+++ packages/react/src/merge-refs/index.ts
@@ -0,0 +1,15 @@
+function mergeRefs<T = any>(
+  refs: Array<React.RefAttributes<T>["ref"] | undefined | null>,
+): React.RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.RefObject<T | null>).current = value;
+      }
+    });
+  };
+}
+
+export default mergeRefs;
