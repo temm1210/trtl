a370cb64108c44b8b07215d3091ec303a827c474
temm1210 <temm1210@naver.com>
2025-01-17 20:34:52 +0900

    feat(ui): create test utils

---
+++ packages/react/src/test/utils.tsx
@@ -0,0 +1,15 @@
+import { RenderOptions, render as rtlRender } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+export function render(
+  ui: React.ReactElement,
+  options?: RenderOptions,
+): ReturnType<typeof rtlRender> & {
+  userEvent: ReturnType<typeof userEvent.setup>;
+} {
+  const user = userEvent.setup({ delay: null });
+
+  const result = rtlRender(ui, options);
+
+  return { userEvent: user, ...result };
+}
