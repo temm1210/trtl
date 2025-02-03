afc807ae2f30417a30000fe19ada11917c8a55da
temm1210 <temm1210@naver.com>
2025-02-03 17:07:16 +0900

    test(react): add mergeRefs test code

---
+++ packages/react/src/merge-refs/index.test.tsx
@@ -0,0 +1,27 @@
+import { render } from "@/test/utils";
+
+import mergeRefs from ".";
+
+it("mergeRefs call with ref, null, undefined", async () => {
+  const ref1 = { current: undefined };
+  const ref2 = jest.fn();
+
+  const Component = () => {
+    return (
+      <div
+        data-testid="merge-ref-test"
+        ref={mergeRefs([ref1, ref2, null, undefined])}
+      >
+        test
+      </div>
+    );
+  };
+
+  const { findByTestId } = render(<Component />);
+
+  const element = await findByTestId("merge-ref-test");
+
+  expect(ref1.current).toBe(element);
+  expect(ref2).toHaveBeenCalledTimes(1);
+  expect(ref2).toHaveBeenCalledWith(element);
+});
