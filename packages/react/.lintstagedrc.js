44c070c03730641a26d4c9d5ce763b92952dbe5c
temm1210 <temm1210@naver.com>
2025-02-03 17:48:18 +0900

    chore: add test script in lint staged

--- packages/react/.lintstagedrc.js
+++ packages/react/.lintstagedrc.js
@@ -2,6 +2,7 @@ export default {
   "*.{js,jsx,ts,tsx}": [
     "eslint --fix",
     () => "tsc --project . --noEmit",
+    "jest --bail --findRelatedTests --passWithNoTests",
     "prettier --write",
   ],
 };
