f02783a2144e6ac63933c2e32fdf301dc9445e64
taekwoo <temm1210@naver.com>
2025-04-18 03:03:09 +0900

    feat(primitives): update checkbox primitive frame

--- packages/react/primitives/src/checkbox/checkbox-primitive.tsx
+++ packages/react/primitives/src/checkbox/checkbox-primitive.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { createContext } from "@rtl/react-utils";
 
 interface CheckboxContextValue {
@@ -7,22 +9,67 @@ interface CheckboxContextValue {
 
 const [CheckboxProvider, useCheckboxContext] = createContext<CheckboxContextValue>();
 
-export interface CheckboxProps extends React.ComponentPropsWithRef<"input"> {
+export interface CheckboxRootProps extends React.ComponentPropsWithRef<"input"> {
   onCheckedChange?: (checked: boolean) => void;
 }
 
-const CheckboxRoot = () => {
+const CheckboxRoot = ({
+  checked: checkedProp,
+  defaultChecked,
+  disabled,
+  onCheckedChange,
+  onChange,
+  ...inputProps
+}: CheckboxRootProps) => {
+  const [clickedState, setClickedState] = useState(defaultChecked ?? false);
+
+  const isControlled = checkedProp !== undefined;
+  const checked = isControlled ? checkedProp : clickedState;
+
+  const dataState = checked ? "checked" : "unchecked";
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.currentTarget;
+
+    if (!isControlled) {
+      setClickedState(checked);
+    }
+    onCheckedChange?.(checked);
+    onChange?.(e);
+  };
+
   return (
-    <CheckboxProvider value={{ state: "unchecked", disabled: false }}>
-      <button />
-      <input />
+    <CheckboxProvider value={{ state: dataState, disabled }}>
+      <label>
+        <input
+          type="checkbox"
+          onChange={handleOnChange}
+          defaultChecked={defaultChecked}
+          checked={checked}
+          disabled={disabled}
+          {...inputProps}
+        />
+      </label>
     </CheckboxProvider>
   );
+};
+
+const CheckboxHiddenInput = () => {
+  return <span>indicator</span>;
 };
 
 const CheckboxIndicator = () => {
   return <span>indicator</span>;
 };
 
-export default { Indicator: CheckboxIndicator, Root: CheckboxRoot };
+const CheckboxLabel = () => {
+  return <span>indicator</span>;
+};
+
+export default {
+  Indicator: CheckboxIndicator,
+  Root: CheckboxRoot,
+  HiddenInput: CheckboxHiddenInput,
+  Label: CheckboxLabel,
+};
 export { useCheckboxContext };
--- packages/react/primitives/src/checkbox/index.ts
+++ packages/react/primitives/src/checkbox/index.ts
@@ -1,2 +1,2 @@
+export type * from "./checkbox-primitive";
 export { default as CheckboxPrimitive, useCheckboxContext } from "./checkbox-primitive";
-export type { CheckboxProps } from "./checkbox-primitive";
