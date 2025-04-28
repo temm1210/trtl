733bbb6ba85a2a3ff73c78619264f9cc812f3ae6
taekwoo <temm1210@naver.com>
2025-04-28 20:55:16 +0900

    feat(ci): add workflow

--- packages/react/utils/src/merge-refs/index.ts
+++ packages/react/utils/src/merge-refs/index.ts
@@ -2,13 +2,12 @@ export function mergeRefs<T = any>(
   refs: Array<React.RefAttributes<T>["ref"] | undefined | null>,
 ): React.RefCallback<T> {
   return (value) => {
-    refs.forEach(() => {
-      console.log("value:", value);
-      // if (typeof ref === "function") {
-      //   ref(value);
-      // } else if (ref != null) {
-      //   (ref as React.RefObject<T | null>).current = value;
-      // }
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.RefObject<T | null>).current = value;
+      }
     });
   };
 }
